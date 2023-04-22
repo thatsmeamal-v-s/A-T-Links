1dd045fe9765c5070f5aef581ae84520cc5f292d
thatsmeamal-v-s <thatsmeamal@gmail.com>
2023-04-23 01:11:31 +0530

    made navbar responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,28 +1,77 @@
-import React from 'react'
-import logo from '../../images/logo.png'
-import chevrondown from '../../images/chevrondown.svg'
-import './Navbar.css';
+import React from "react";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
+import logo from "../../images/logo.png";
+import chevrondown from "../../images/chevrondown.svg";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <>
+      <nav className="navbar">
         <div className="logo-container">
-            <img className="logo" alt="logo" src={logo}></img>
+          <img className="logo" alt="logo" src={logo}></img>
         </div>
         <div className="search-container">
-            <input className="form-control mr-sm-2 search" type="search" placeholder="Search..." aria-label="Search"/>
+          <input
+            className="form-control mr-sm-2 search"
+            type="search"
+            placeholder="Search..."
+            aria-label="Search"
+          />
         </div>
+        <button type="button" className="btn btn-dark checkout">
+          <span>Checkout (200)</span>
+        </button>
         <div className="avatar-container">
-            <button type="button" className="btn btn-dark checkout">
-                <span>Checkout (200)</span>
-            </button>
-            <img className="rounded-circle avatar" src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" alt="Avatar" />
-            <span>User Admin</span>
-            <img className="avatar-dropdown" alt="logo" src={chevrondown}></img>
+          <img
+            className="rounded-circle avatar"
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            alt="Avatar"
+          />
+          <span>User Admin</span>
+          <img className="avatar-dropdown" alt="logo" src={chevrondown}></img>
         </div>
+      </nav>
+      <div className="hamburger">
+        <div className="logo-container">
+          <img className="logo" alt="logo" src={logo}></img>
+          <div className="avatar-container-sm">
+            <img
+              className="rounded-circle avatar hamburger-avatar"
+              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+              alt="Avatar"
+              onClick={() => myFunction()}
+            />
+          </div>
+        </div>
+        <div id="small-view" className="hamburger-list">
+          <ul>
+            <li>
+              <input
+                className="form-control mr-sm-2 search"
+                type="search"
+                placeholder="Search..."
+                aria-label="Search"
+              />
+            </li>
+            <li className="sm-cart-container">
+              <AiOutlineShoppingCart className="sm-cart" />
+              <span>Checkout (200)</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    </nav>
-  )
+function myFunction() {
+  var x = document.getElementById("small-view");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
-export default Navbar
+export default Navbar;
