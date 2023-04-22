64dd1170ff82cfa08f136c77e87fee7b85ee80ca
thatsmeamal-v-s <thatsmeamal@gmail.com>
2023-04-22 16:17:16 +0530

    added grid property to cards

--- src/Components/Aboutus/AboutusCards/AboutusCards.js
+++ src/Components/Aboutus/AboutusCards/AboutusCards.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import './AboutusCards.css'
-import ContactCard from './ContactCard/ContactCard'
-import AddressCard from './AddressCard/AddressCard'
-import OperationHoursCard from './OperationHoursCard/OperationHoursCard'
-import SocialMediaCard from './SocialMediaCard/SocialMediaCard'
-import StatementCard from './StatementCard/StatementCard'
+import React from "react";
+import "./AboutusCards.css";
+import ContactCard from "./ContactCard/ContactCard";
+import AddressCard from "./AddressCard/AddressCard";
+import OperationHoursCard from "./OperationHoursCard/OperationHoursCard";
+import SocialMediaCard from "./SocialMediaCard/SocialMediaCard";
+import StatementCard from "./StatementCard/StatementCard";
 
 const AboutusCards = (props) => {
   return (
-    <div className="row">
-      <ContactCard 
-        showPanel={props.showPanel} toggleShowPanel={props.toggleShowPanel}
+    <div className="card-container">
+      <ContactCard
+        showPanel={props.showPanel}
+        toggleShowPanel={props.toggleShowPanel}
         contactList={props.contactList}
       />
-      <AddressCard/>
-      <OperationHoursCard/>
-      <SocialMediaCard/>
-      <StatementCard/>
+      <AddressCard />
+      <OperationHoursCard />
+      <SocialMediaCard />
+      <StatementCard />
     </div>
-  )
-}
+  );
+};
 
-export default AboutusCards
+export default AboutusCards;
