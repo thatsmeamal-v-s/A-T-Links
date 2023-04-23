1eb9e6b44d40846be4ba7421396a1c8b2fa2a4a8
thatsmeamal-v-s <thatsmeamal@gmail.com>
2023-04-23 12:54:18 +0530

    made aboutus navbar responsive

--- src/Components/Aboutus/AboutusNavbar/AboutusNavbar.js
+++ src/Components/Aboutus/AboutusNavbar/AboutusNavbar.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import './AboutusNavbar.css'
+import React from "react";
+import "./AboutusNavbar.css";
 
 const AboutusNavbar = () => {
   return (
     <div className="aboutus-navbar-container">
-        <ul className="aboutus-navbar">
-          <li><span className="aboutus-navbar-active">Info</span></li>
-          <li><span>FAQ</span></li>
-          <li><span>Complaints and Feedback</span></li>
-          <li><span>Privacy Policy</span></li>
-          <li><span>Terms & Conditions</span></li>
-        </ul>
+      <ul className="aboutus-navbar">
+        <li>
+          <span className="aboutus-navbar-active">Info</span>
+        </li>
+        <li>
+          <span>FAQ</span>
+        </li>
+        <li>
+          <span>Complaints and Feedback</span>
+        </li>
+        <li>
+          <span>Privacy Policy</span>
+        </li>
+        <li>
+          <span>Terms & Conditions</span>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default AboutusNavbar
+export default AboutusNavbar;
